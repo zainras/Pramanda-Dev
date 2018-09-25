a30ea2c2926cccc295210f361c265a55e2cd5c68
zainras <zain95394@gmail.com>
2018-09-25 23:07:41 +0700

    tambah activiy map level 1

--- app/src/main/java/dev/mandapo/pramandagame/MainActivity.java
+++ app/src/main/java/dev/mandapo/pramandagame/MainActivity.java
@@ -1,7 +1,9 @@
 package dev.mandapo.pramandagame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startLv1(View view) {
+        Intent intent = new Intent(MainActivity.this, MapLv1.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/dev/mandapo/pramandagame/MapLv1.java
@@ -0,0 +1,13 @@
+package dev.mandapo.pramandagame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapLv1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_lv1);
+    }
+}
